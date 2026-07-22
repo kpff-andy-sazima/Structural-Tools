1ec6a8612e12d888afb52dc03261ea20a259d3d4
Andy <Andy.Sazima@kpff.com>
2026-07-22 10:39:29 -0700

    update model load combo algo

--- src/structural_tools/model.py
+++ src/structural_tools/model.py
@@ -10,6 +10,18 @@ from Pynite import FEModel3D
 
 from .asce import DesignMethod
 
+from enum import Enum, auto
+
+
+class PrincipalLoad(Enum):
+    DEAD = auto()
+    LIVE = auto()
+    LIVE_ROOF = auto()
+    SNOW = auto()
+    RAIN = auto()
+    WIND = auto()
+    SEISMIC = auto()
+
 
 @dataclass
 class ModelMaterial:
@@ -59,16 +71,20 @@ MATERIAL_WOOD_KIP_INCH: ModelMaterial = ModelMaterial(
 
 def create_base_model(
     design_method: DesignMethod = DesignMethod.LRFD,
+    principal_loads: PrincipalLoad | list[PrincipalLoad] | None = None,
+    materials: ModelMaterial | list[ModelMaterial] = MATERIAL_STEEL_50_KSI_KIP_INCH,
     include_ossc_service_combos: bool = True,
     include_ossc_creep_combos: bool = True,
     include_asce_service_combos: bool = False,
     include_asce_creep_combos: bool = False,
-    materials: ModelMaterial | list[ModelMaterial] = MATERIAL_STEEL_50_KSI_KIP_INCH,
 ) -> FEModel3D:
     base_model = FEModel3D()
 
-    if not isinstance(materials, list):
+    if isinstance(materials, ModelMaterial):
         materials = [materials]
+
+    if isinstance(principal_loads, PrincipalLoad):
+        principal_loads = [principal_loads]
 
     for material in materials:
         base_model.add_material(
@@ -76,66 +92,126 @@ def create_base_model(
         )
 
     # Load combinations
+    load_combinations = []
+
+    # Baseline load combinations
+    load_combinations += [
+        ("D", {"D": 1.0}, ["self weight", "debug"]),
+        (
+            "all",
+            {
+                "D": 1.0,
+                "L": 1.0,
+                "L_r": 1.0,
+                "S": 1.0,
+                "W": 1.0,
+                "R": 1.0,
+                "E_v": 1.0,
+                "E_h": 1.0,
+            },
+            ["all nominal", "debug"],
+        ),
+    ]
+
     # LRFD combos
     if design_method == DesignMethod.LRFD:
-        base_model.add_load_combo("1a", {"D": 1.4}, ["strength", "principal D"])
-        base_model.add_load_combo("2a-1", {"D": 1.2, "L": 1.6, "L_r": 0.5}, ["strength", "principal L"])
-        base_model.add_load_combo("2a-2", {"D": 1.2, "L": 1.6, "S": 0.3}, ["strength", "principal L"])
-        base_model.add_load_combo("2a-3", {"D": 1.2, "L": 1.6, "R": 0.5}, ["strength", "principal L"])
-        base_model.add_load_combo("3a-1", {"D": 1.2, "L_r": 1.6, "L": 1.0}, ["strength", "principal L_r"])
-        base_model.add_load_combo("3a-2", {"D": 1.2, "S": 1.0, "L": 1.0}, ["strength", "principal S"])
-        base_model.add_load_combo("3a-3", {"D": 1.2, "R": 1.6, "L": 1.0}, ["strength", "principal R"])
-        base_model.add_load_combo("3a-4", {"D": 1.2, "L_r": 1.6, "W": 0.5}, ["strength", "principal L_r"])
-        base_model.add_load_combo("3a-5", {"D": 1.2, "S": 1.0, "W": 0.5}, ["strength", "principal S"])
-        base_model.add_load_combo("3a-6", {"D": 1.2, "R": 1.6, "W": 0.5}, ["strength", "principal R"])
-        base_model.add_load_combo("4a-1", {"D": 1.2, "W": 1.0, "L": 1.0, "L_r": 0.5}, ["strength", "principal W"])
-        base_model.add_load_combo("4a-2", {"D": 1.2, "W": 1.0, "L": 1.0, "S": 0.3}, ["strength", "principal W"])
-        base_model.add_load_combo("4a-3", {"D": 1.2, "W": 1.0, "L": 1.0, "R": 0.5}, ["strength", "principal W"])
-        base_model.add_load_combo("5a", {"D": 0.9, "W": -1.0}, ["strength", "principal W"])
-        base_model.add_load_combo(
-            "6", {"D": 1.2, "E_v": 1.0, "E_h": 1.0, "L": 1.0, "S": 0.15}, ["strength", "principal E"]
-        )
-        base_model.add_load_combo("7", {"D": 0.9, "E_v": -1.0, "E_h": 1.0}, ["strength", "principal E"])
+        if PrincipalLoad.DEAD in principal_loads:
+            load_combinations += [("1a", {"D": 1.4}, ["strength", "principal D"])]
+        if PrincipalLoad.LIVE in principal_loads:
+            load_combinations += [
+                ("2a-1", {"D": 1.2, "L": 1.6, "L_r": 0.5}, ["strength", "principal L"]),
+                ("2a-2", {"D": 1.2, "L": 1.6, "S": 0.3}, ["strength", "principal L"]),
+                ("2a-3", {"D": 1.2, "L": 1.6, "R": 0.5}, ["strength", "principal L"]),
+            ]
+        if PrincipalLoad.LIVE_ROOF in principal_loads:
+            load_combinations += [
+                ("3a-1", {"D": 1.2, "L_r": 1.6, "L": 1.0}, ["strength", "principal L_r"]),
+                ("3a-4", {"D": 1.2, "L_r": 1.6, "W": 0.5}, ["strength", "principal L_r"]),
+            ]
+        if PrincipalLoad.SNOW in principal_loads:
+            load_combinations += [
+                ("3a-2", {"D": 1.2, "S": 1.0, "L": 1.0}, ["strength", "principal S"]),
+                ("3a-5", {"D": 1.2, "S": 1.0, "W": 0.5}, ["strength", "principal S"]),
+            ]
+        if PrincipalLoad.RAIN in principal_loads:
+            load_combinations += [
+                ("3a-3", {"D": 1.2, "R": 1.6, "L": 1.0}, ["strength", "principal R"]),
+                ("3a-6", {"D": 1.2, "R": 1.6, "W": 0.5}, ["strength", "principal R"]),
+            ]
+        if PrincipalLoad.WIND in principal_loads:
+            load_combinations += [
+                ("4a-1", {"D": 1.2, "W": 1.0, "L": 1.0, "L_r": 0.5}, ["strength", "principal W"]),
+                ("4a-2", {"D": 1.2, "W": 1.0, "L": 1.0, "S": 0.3}, ["strength", "principal W"]),
+                ("4a-3", {"D": 1.2, "W": 1.0, "L": 1.0, "R": 0.5}, ["strength", "principal W"]),
+                ("5a", {"D": 0.9, "W": -1.0}, ["strength", "principal W"]),
+            ]
+        if PrincipalLoad.SEISMIC in principal_loads:
+            load_combinations += [
+                ("6", {"D": 1.2, "E_v": 1.0, "E_h": 1.0, "L": 1.0, "S": 0.15}, ["strength", "principal E"]),
+                ("7", {"D": 0.9, "E_v": -1.0, "E_h": 1.0}, ["strength", "principal E"]),
+            ]
+    elif design_method == DesignMethod.ASD:
+        if PrincipalLoad.DEAD in principal_loads:
+            load_combinations += [("1a", {"D": 1.0}, ["strength", "principal D"])]
+        if PrincipalLoad.LIVE in principal_loads:
+            load_combinations += [
+                ("2a", {"D": 1.0, "L": 1.0}, ["strength", "principal L"]),
+                ("4a-1", {"D": 1.0, "L": 0.75, "L_r": 0.75}, ["strength", "principal L"]),
+                ("4a-2", {"D": 1.0, "L": 0.75, "S": 0.525}, ["strength", "principal L"]),
+                ("4a-3", {"D": 1.0, "L": 0.75, "R": 0.75}, ["strength", "principal L"]),
+            ]
+        if PrincipalLoad.LIVE_ROOF in principal_loads:
+            load_combinations += [
+                ("3a-1", {"D": 1.0, "L_r": 1.0}, ["strength", "principal L_r"]),
+            ]
+        if PrincipalLoad.SNOW in principal_loads:
+            load_combinations += [
+                ("3a-2", {"D": 1.0, "S": 0.7}, ["strength", "principal S"]),
+            ]
+        if PrincipalLoad.RAIN in principal_loads:
+            load_combinations += [
+                ("3a-3", {"D": 1.0, "R": 1.0}, ["strength", "principal R"]),
+            ]
+        if PrincipalLoad.WIND in principal_loads:
+            load_combinations += [
+                ("5a", {"D": 1.0, "W": 0.6}, ["strength", "principal W"]),
+                ("6a-1", {"D": 1.0, "L": 0.75, "W": 0.45, "L_r": 0.75}, ["strength", "principal W"]),
+                ("6a-2", {"D": 1.0, "L": 0.75, "W": 0.45, "S": 0.525}, ["strength", "principal W"]),
+                ("6a-3", {"D": 1.0, "L": 0.75, "W": 0.45, "R": 0.75}, ["strength", "principal W"]),
+                ("7a", {"D": 0.6, "W": -0.6}, ["strength", "principal W"]),
+            ]
+        if PrincipalLoad.SEISMIC in principal_loads:
+            load_combinations += [
+                ("8", {"D": 1.0, "E_v": 0.7, "E_h": 0.7}, ["strength", "principal E"]),
+                ("9", {"D": 1.0, "E_v": 0.525, "E_h": 0.525, "L": 0.75, "S": 0.1}, ["strength", "principal E"]),
+                ("10", {"D": 0.6, "E_v": -0.7, "E_h": 0.7}, ["strength", "principal E"]),
+            ]
     else:
-        base_model.add_load_combo("1a", {"D": 1.0}, ["strength", "principal D"])
-        base_model.add_load_combo("2a", {"D": 1.0, "L": 1.0}, ["strength", "principal L"])
-        base_model.add_load_combo("3a-1", {"D": 1.0, "L_r": 1.0}, ["strength", "principal L_r"])
-        base_model.add_load_combo("3a-2", {"D": 1.0, "S": 0.7}, ["strength", "principal S"])
-        base_model.add_load_combo("3a-3", {"D": 1.0, "R": 1.0}, ["strength", "principal R"])
-        base_model.add_load_combo("4a-1", {"D": 1.0, "L": 0.75, "L_r": 0.75}, ["strength", "principal L"])
-        base_model.add_load_combo("4a-2", {"D": 1.0, "L": 0.75, "S": 0.525}, ["strength", "principal L"])
-        base_model.add_load_combo("4a-3", {"D": 1.0, "L": 0.75, "R": 0.75}, ["strength", "principal L"])
-        base_model.add_load_combo("5a", {"D": 1.0, "W": 0.6}, ["strength", "principal W"])
-        base_model.add_load_combo("6a-1", {"D": 1.0, "L": 0.75, "W": 0.45, "L_r": 0.75}, ["strength", "principal W"])
-        base_model.add_load_combo("6a-2", {"D": 1.0, "L": 0.75, "W": 0.45, "S": 0.525}, ["strength", "principal W"])
-        base_model.add_load_combo("6a-3", {"D": 1.0, "L": 0.75, "W": 0.45, "R": 0.75}, ["strength", "principal W"])
-        base_model.add_load_combo("7a", {"D": 0.6, "W": -0.6}, ["strength", "principal W"])
-        base_model.add_load_combo("8", {"D": 1.0, "E_v": 0.7, "E_h": 0.7}, ["strength", "principal E"])
-        base_model.add_load_combo(
-            "9", {"D": 1.0, "E_v": 0.525, "E_h": 0.525, "L": 0.75, "S": 0.1}, ["strength", "principal E"]
-        )
-        base_model.add_load_combo("10", {"D": 0.6, "E_v": -0.7, "E_h": 0.7}, ["strength", "principal E"])
+        raise ValueError("Design Method should be LRFD or ASD")
 
     # Add service load combos
     if include_ossc_service_combos:
-        base_model.add_load_combo("L", {"L": 1.0}, ["service"])
-        base_model.add_load_combo("L_r", {"L_r": 1.0}, ["service"])
-        base_model.add_load_combo("S", {"S": 1.0}, ["service"])
-        base_model.add_load_combo("W", {"W": 1.0}, ["service"])
+        load_combinations += [
+            ("L OSSC25", {"L": 1.0}, ["service OSSC25"]),
+            ("L_r OSSC25", {"L_r": 1.0}, ["service OSSC25"]),
+            ("S OSSC25", {"S": 1.0}, ["service OSSC25"]),
+            ("W OSSC25", {"W": 1.0}, ["service OSSC25"]),
+        ]
     if include_ossc_creep_combos:
-        base_model.add_load_combo("0.5D+L", {"D": 0.5, "L": 1.0}, ["creep OSSC25 dry wood"])
-        base_model.add_load_combo("D+L", {"D": 1.0, "L": 1.0}, ["creep OSSC25 moist wood"])
+        load_combinations += [
+            ("0.5D+L OSSC25", {"D": 0.5, "L": 1.0}, ["creep OSSC25 dry wood"]),
+            ("D+L OSSC25", {"D": 1.0, "L": 1.0}, ["creep OSSC25 moist wood"]),
+        ]
     if include_asce_service_combos:
-        base_model.add_load_combo("CC.2-1a", {"D": 1.0, "L": 1.0}, ["service ASCE7-22"])
-        base_model.add_load_combo("CC.2-1b", {"D": 1.0, "S_ser": 1.0}, ["service ASCE7-22"])
+        load_combinations += [
+            ("CC.2-1a ASCE7-22", {"D": 1.0, "L": 1.0}, ["service ASCE7-22"]),
+            ("CC.2-1b ASCE7-22", {"D": 1.0, "S_ser": 1.0}, ["service ASCE7-22"]),
+        ]
     if include_asce_creep_combos:
-        base_model.add_load_combo("CC.2-2", {"D": 1.0, "L": 0.5}, ["creep ASCE7-22"])
+        load_combinations += [("CC.2-2 ASCE7-22", {"D": 1.0, "L": 0.5}, ["creep ASCE7-22"])]
 
-    # Other load combos
-    base_model.add_load_combo("D", {"D": 1.0}, ["self weight"])
-    base_model.add_load_combo(
-        "all", {"D": 1.0, "L": 1.0, "L_r": 1.0, "S": 1.0, "W": 1.0, "R": 1.0, "E_v": 1.0, "E_h": 1.0}, ["all nominal"]
-    )
+    for name, factors, tags in load_combinations:
+        base_model.add_load_combo(name, factors, tags)
 
     return deepcopy(base_model)
 
