dc9fa1e9b4cd9bec6b8a6ae3127aff814f7c073c
Andy <Andy.Sazima@kpff.com>
2026-08-11 16:01:07 -0700

    code versions

--- src/structural_tools/asce/general.py
+++ src/structural_tools/asce/general.py
@@ -4,3 +4,15 @@ from enum import Enum
 class DesignMethod(Enum):
     ASD = "ASD"
     LRFD = "LRFD"
+
+class CodeVersion(Enum):
+    AISC_360_16 = 2016
+    AISC_360_18 = 2018
+    AISC_360_22 = 2022
+    ASCE_7_16 = 2016
+    ASCE_7_22 = 2022
+    NDS_2015 = 2015
+    NDS_2018 = 2018
+    NDS_2024 = 2024
+    SDPWS_2015 = 2015
+    SDPWS_2021 = 2021
