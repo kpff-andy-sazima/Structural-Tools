b6202de4e66243903bcf33efb5a39db4d44c59b5
Andy <Andy.Sazima@kpff.com>
2026-08-11 17:34:21 -0700

    better init and import support

--- src/structural_tools/asce/__init__.py
+++ src/structural_tools/asce/__init__.py
@@ -1,4 +1,19 @@
 from .general import DesignMethod
-from .seismic_parameters import RiskCategory, SeismicDesignCategory, SeismicParameters, SiteClass
+from .seismic_parameters import (
+    RiskCategory,
+    SeismicDesignCategory,
+    SeismicParameters,
+    SiteClass,
+    LOAD_FACTOR_SEISMIC_ASD,
+    LOAD_FACTOR_SEISMIC_LRFD,
+)
 
-__all__ = ["DesignMethod", "RiskCategory", "SeismicDesignCategory", "SeismicParameters", "SiteClass"]
+__all__ = [
+    "DesignMethod",
+    "RiskCategory",
+    "SeismicDesignCategory",
+    "SeismicParameters",
+    "SiteClass",
+    "LOAD_FACTOR_SEISMIC_ASD",
+    "LOAD_FACTOR_SEISMIC_LRFD",
+]
--- src/structural_tools/notebook/__init__.py
+++ src/structural_tools/notebook/__init__.py
@@ -1,14 +1,36 @@
-import units
-import handcalcs
+import forallpeople as units
+from handcalcs.global_config import set_option
 from .plotting import set_plot_style
+from .calculation import check_value
+from .display import (
+    display_figure,
+    display_markdown,
+    display_math,
+    display_table,
+    display_text,
+    set_params_columns,
+    sig_figs,
+)
 
 
 def initialize_notebook() -> None:
     units.environment(env_name="structural", top_level=True)
-    handcalcs.set_option("custom_symbols", {"star": "^*", "__": ",", "plus": "+", "minus": "-"})
-    handcalcs.set_option("greek_exclusions", ["psi"])
-    handcalcs.set_option("display_precision", 4)
-    handcalcs.set_option("param_columns", 1)
-    handcalcs.set_option("latex_block_start", "\\begin{equation*}")
-    handcalcs.set_option("latex_block_end", "\\end{equation*}")
+    set_option("custom_symbols", {"star": "^*", "__": ",", "plus": "+", "minus": "-"})
+    set_option("greek_exclusions", ["psi"])
+    set_option("display_precision", 4)
+    set_option("param_columns", 1)
+    set_option("latex_block_start", "\\begin{equation*}")
+    set_option("latex_block_end", "\\end{equation*}")
     set_plot_style()
+
+
+__all__ = [
+    "check_value",
+    "display_figure",
+    "display_markdown",
+    "display_math",
+    "display_table",
+    "display_text",
+    "set_params_columns",
+    "sig_figs",
+]
