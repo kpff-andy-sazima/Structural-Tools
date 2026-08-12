6a77f242ec0d8be7527e57088b95358f2091686d
Andy <Andy.Sazima@kpff.com>
2026-08-11 17:51:25 -0700

    unit stubs

--- src/structural_tools/notebook/__init__.py
+++ src/structural_tools/notebook/__init__.py
@@ -14,14 +14,16 @@ from .display import (
 
 
 def initialize_notebook() -> None:
-    units.environment(env_name="structural", top_level=True)
     set_option("custom_symbols", {"star": "^*", "__": ",", "plus": "+", "minus": "-"})
     set_option("greek_exclusions", ["psi"])
     set_option("display_precision", 4)
     set_option("param_columns", 1)
     set_option("latex_block_start", "\\begin{equation*}")
     set_option("latex_block_end", "\\end{equation*}")
+
     set_plot_style()
+
+    units.environment(env_name="structural", top_level=True)
 
 
 __all__ = [
